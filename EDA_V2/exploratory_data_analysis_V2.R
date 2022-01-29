9662ac015891334f5016bb1d2efb9968fc0e2e29
ceciliazagni <ceciliazagni@gmail.com>
2022-01-29 19:44:26 +0100

    add what discussed on 29/01 meeting

--- EDA_V2/exploratory_data_analysis_V2.R
+++ EDA_V2/exploratory_data_analysis_V2.R
@@ -10,15 +10,28 @@ library(corrplot)
 library(PerformanceAnalytics)
 library(car)
 library(ggplot2)
+library(hrbrthemes)
+library(ggpubr)
+library(dplyr)
+
+
+
+###############################################################################
+#                          DATASET PREPARATION                                #
+###############################################################################
 
 # upload and prepare the dataset cov
-cov<-read.csv("https://raw.githubusercontent.com/pcm-dpc/
-COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
-#view(cov)
+#cov<-read.csv("https://raw.githubusercontent.com/pcm-dpc/
+#COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv")
+##view(cov)
+#write.csv(cov,"Covid19.csv")
+
+# import the dataset
+cov_orig <- read.csv("Covid19.csv")
 
 ### in this project we are going to work with the region Campania, Intensive Care Units (terapia_intensiva) )
 # select the variables/period of interest
-cov<-filter(cov,denominazione_regione=="Campania")
+cov<-filter(cov_orig,denominazione_regione=="Campania")
 nrow(cov)
 names(cov)
 #view(cov)
@@ -29,143 +42,278 @@ cov<-select(cov,data,ricoverati_con_sintomi,terapia_intensiva,ingressi_terapia_i
 sum(is.na(cov$data))
 cov$data<-as.Date(cov$data)
 #view(cov)
-cov<-filter(cov,cov$data>="2020-10-01",cov$data<="2021-02-01")
+cov<-filter(cov,cov$data>="2020-09-30",cov$data<="2021-02-01")
 #view(cov)
 nrow(cov)
 sum(is.na(cov$ingressi_terapia_intensiva))
 cov<-select(cov, -ingressi_terapia_intensiva)
 #view(cov)
+cov <- cov[order(cov$data),]
+cov$deceduti_daily <- c(cov$deceduti[1],diff(cov$deceduti))
+cov$dimessi_guariti_dayly <- c(cov$dimessi_guariti[1],diff(cov$dimessi_guariti))
+cov$casi_testati_daily <- c(cov$casi_testati[1],diff(cov$casi_testati))
+cov<-filter(cov,cov$data>="2020-10-01",cov$data<="2021-02-01")
+cov$nuovi_positivi_norm <- cov$nuovi_positivi/cov$casi_testati_daily
+
 
 # upload and prepare the dataset vax and add it to cov
-vax <- read_csv("Vax.csv")
-names(vax)
-vax <- subset(vax,vax$nome_area == "Campania")
-vax <- select(vax,data_somministrazione,totale,prima_dose,seconda_dose,pregressa_infezione)
+# https://github.com/pcm-dpc/COVID-19 ???
+vax_orig <- read_csv("Vax.csv")
+names(vax_orig)
+vax <- subset(vax_orig,vax_orig$nome_area == "Campania")
+vax <- select(vax,data_somministrazione,totale)
 vax <- subset(vax,vax$data_somministrazione <= '2021-02-01')
 vax <- rename(vax,dayly_vax = totale)
-vax$totale_vax <- cumsum(vax$dayly_vax)
-cov_vax <- merge(cov,vax, by.x = "data", by.y = "data_somministrazione", all.x = TRUE)
-cov_vax[is.na(cov_vax)] <- 0
+vax <- vax[order(vax$data_somministrazione),]
+vax$tot_vax <- cumsum(vax$dayly_vax)
+pop_campania = 5624260 #https://www.tuttitalia.it/campania/statistiche/popolazione-andamento-demografico/
+vax$perc_vax <- vax$tot_vax/pop_campania*100
+cov <- merge(cov,vax, by.x = "data", by.y = "data_somministrazione", all.x = TRUE)
+cov[is.na(cov)] <- 0
+# for now I choose to consider only %of vax, then we can choose what we prefer
+cov <- select(cov, -tot_vax, -dayly_vax)
+
+# upload ad add colore dataset
+# https://github.com/imcatta/restrizioni_regionali_covid
+color_orig <- read_csv("color.csv")
+color <- subset(color_orig,color_orig$denominazione_regione == "Campania")
+names(color)
+color <- select(color,"data","colore")
+color <- subset(color,color$data <= '2021-02-01')
+color <- subset(color,color$data >= '2020-10-01')
+color <- color[order(color$data),]
+cov <- merge(cov,color, by.x = "data", by.y = "data", all.x = TRUE)
+cov[is.na(cov)] <- "pre_decreto"
+cov$colore <- as.factor(cov$colore)
 
 
-# plot the data
-x11()
-pairs(cov)
+
+###############################################################################
+#            SHOW THE VARIABLES AND CHOOSE WHICH ONE TO CONSIDER              #
+###############################################################################
 
 # plot the dependencies between ricoverati_con_sintomi terapia_intensiva totale_ospedalizzati
-x11()
-plot(cov$data,cov$ricoverati_con_sintomi,ylim = c(0,2500), col = "green")
-lines(cov$data,cov$terapia_intensiva, col = "blue")
-lines(cov$data,cov$totale_ospedalizzati, col = "red")
-
-p1 <- ggplot(cov, aes(x=data)) +
-  geom_line(aes(y = ricoverati_con_sintomi, colour = "ric")) +
-  geom_line(aes(y = terapia_intensiva, colour = "ter")) +
-  geom_line(aes(y = totale_ospedalizzati, colour = "tot")) +
-  geom_density(adjust=2) +
+p1 <- ggplot(cov) +
+  geom_line(mapping = aes(x=data, y = ricoverati_con_sintomi, color = "ricoverati_con_sintomi")) +
+  geom_line(mapping = aes(x=data, y = terapia_intensiva, color = "terapia_intensiva")) +
+  geom_line(mapping = aes(x=data, y = totale_ospedalizzati, color = "totale_ospedalizzati")) +
   theme_ipsum()
 
 x11()
 p1
-
 # we can discard totale_ospedalizzati
 
+
 # same with totale_ospedalizzati isolamento_domiciliare totale_positivi
-p2 <- ggplot(cov, aes(x=data)) +
-  geom_line(aes(y = totale_ospedalizzati, colour = "osp")) +
-  geom_line(aes(y = isolamento_domiciliare, colour = "casa")) +
-  geom_line(aes(y = totale_positivi, colour = "tot")) +
-  geom_density(adjust=2) +
+p2 <- ggplot(cov) +
+  geom_line(mapping = aes(x=data, y = totale_ospedalizzati, color = "totale_ospedalizzati")) +
+  geom_line(mapping = aes(x=data, y = isolamento_domiciliare, color = "isolamento_domiciliare")) +
+  geom_line(mapping = aes(x=data, y = totale_positivi, color = "totale_positivi")) +
   theme_ipsum()
 
 x11()
 p2
 
-# we can discard totale_positivi
+# we can discard totale_positivi 
+cov <- select(cov, -totale_ospedalizzati, -totale_positivi)
 
-# should we take a look at all the variables wrt date?
+# let's take a look at all the variables wrt date
+names(cov)
+date_TI <- ggplot(cov, aes(x=data, y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_RCS <- ggplot(cov, aes(x=data, y=ricoverati_con_sintomi, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_ID <- ggplot(cov, aes(x=data, y=isolamento_domiciliare, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_NP <- ggplot(cov, aes(x=data, y=nuovi_positivi, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_DG <- ggplot(cov, aes(x=data, y=dimessi_guariti, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_D <- ggplot(cov, aes(x=data, y=deceduti, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_TC <- ggplot(cov, aes(x=data, y=totale_casi, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_T <- ggplot(cov, aes(x=data, y=tamponi, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_CT <- ggplot(cov, aes(x=data, y=casi_testati, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_PV <- ggplot(cov, aes(x=data, y=perc_vax, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+
+#date_TI
+#date_RCS
+#date_ID
+#date_NP
+#date_DG
+#date_D 
+#date_TC
+#date_T 
+#date_CT
+#date_PV
+
+x11()
+ggarrange(date_TI,date_RCS,date_ID,date_NP,date_DG,date_D,date_TC,
+          date_T,date_CT,date_PV, 
+          ncol = 3, nrow = 4)
+
+# we also choose not to consider totale_casi and tamponi
+# (see Walter Work to see how to consider the casi_testati variable)
+# because they are cumulative variables that we are not particularly interested in
+cov <- select(cov, -totale_casi, -tamponi, -casi_testati)
+
+# we see what happens if we consider deceduti and dimessi_guariti on a daily basis
+date_DGD <- ggplot(cov, aes(x=data, y=dimessi_guariti_dayly, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+date_DD <- ggplot(cov, aes(x=data, y=deceduti_daily, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+
+
+x11()
+ggarrange(date_TI,date_RCS,date_ID,date_NP,date_DGD,date_DD,date_PV, 
+          ncol = 3, nrow = 3)
+
+# should we consider nuovi_positivi or nuovi_positivi normalized?
+date_NPN <- ggplot(cov, aes(x=data, y=nuovi_positivi_norm, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+x11()
+ggarrange(date_NP,date_NPN,
+          ncol = 2, nrow = 1)
+
+
+
 
 # plot terapia_intensiva wrt all the variables
+names(cov)
+date_TI <- ggplot(cov, aes(x=data, y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+RCS_TI <- ggplot(cov, aes(x=ricoverati_con_sintomi, y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+ID_TI <- ggplot(cov, aes(x=isolamento_domiciliare, y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+NP_TI <- ggplot(cov, aes(x=nuovi_positivi,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+DG_TI <- ggplot(cov, aes(x=dimessi_guariti,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+D_TI <- ggplot(cov, aes(x=deceduti,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+PV_TI <- ggplot(cov, aes(x=perc_vax,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+
 x11()
-par(mfrow=c(3,4))
-plot(cov$data,cov$terapia_intensiva)
-plot(cov$ricoverati_con_sintomi,cov$terapia_intensiva)
-#plot(cov$totale_ospedalizzati,cov$terapia_intensiva)
-plot(cov$isolamento_domiciliare,cov$terapia_intensiva)
-#plot(cov$totale_positivi,cov$terapia_intensiva)
-plot(cov$nuovi_positivi,cov$terapia_intensiva)
-plot(cov$dimessi_guariti,cov$terapia_intensiva)
-plot(cov$deceduti,cov$terapia_intensiva)
-plot(cov$totale_casi,cov$terapia_intensiva)
-#plot(cov$tamponi,cov$terapia_intensiva) # explain
-plot(cov$casi_testati,cov$terapia_intensiva)
+ggarrange(date_TI,RCS_TI,ID_TI,NP_TI,DG_TI,D_TI ,PV_TI, 
+          ncol = 3, nrow = 3)
 
 # we notice an outlier
 watchout_point <- max(cov$terapia_intensiva)
+# now we have to choose if we want to exclude the outlier. in my opinion yes
+
+# we see what happens if we consider deceduti and dimessi_guariti on a daily basis
+DGD_TI <- ggplot(cov, aes(x=dimessi_guariti_dayly,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+DD_TI <- ggplot(cov, aes(x=deceduti_daily,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
 
 x11()
-par(mfrow=c(3,4))
-plot(cov$data,cov$terapia_intensiva)
-points(cov$data[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$ricoverati_con_sintomi,cov$terapia_intensiva)
-points(cov$ricoverati_con_sintomi[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$totale_ospedalizzati,cov$terapia_intensiva)
-points(cov$totale_ospedalizzati[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$isolamento_domiciliare,cov$terapia_intensiva)
-points(cov$isolamento_domiciliare[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$totale_positivi,cov$terapia_intensiva)
-points(cov$totale_positivi[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$nuovi_positivi,cov$terapia_intensiva)
-points(cov$nuovi_positivi[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$dimessi_guariti,cov$terapia_intensiva)
-points(cov$dimessi_guariti[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$deceduti,cov$terapia_intensiva)
-points(cov$deceduti[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$totale_casi,cov$terapia_intensiva)
-points(cov$totale_casi[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$tamponi,cov$terapia_intensiva)
-points(cov$tamponi[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
-plot(cov$casi_testati,cov$terapia_intensiva)
-points(cov$casi_testati[which(cov$terapia_intensiva == watchout_point)],watchout_point, col = "red", pch = 16)
+ggarrange(date_TI,RCS_TI,ID_TI,NP_TI,DGD_TI,DD_TI ,PV_TI, 
+          ncol = 3, nrow = 3)
 
-# now we have to choose if we want to exclude the outlier. in my opinion yes
+# should we consider nuovi_positivi or nuovi_positivi normalized?
+NPN_TI <- ggplot(cov, aes(x=nuovi_positivi_norm,y=terapia_intensiva, color=colore)) + 
+  geom_point(size=3) +
+  scale_color_manual(values=c("orange", "yellow", "grey", "red")) +
+  theme_ipsum()
+x11()
+ggarrange(NP_TI,NPN_TI, 
+          ncol = 2, nrow = 1)
+# Discuss together these results
+
+
+
+
+###############################################################################
+#                         CORRELATION ANALYSIS                                #
+###############################################################################
 
 
 # correlation analysis
-corr <- cor(select(cov, -data))
-corr
-x11()
-corrplot(corr, method = "ellipse")
+#corr <- cor(select(cov, -data, -colore))
+#corr
+#x11()
+#corrplot(corr, method = "ellipse")
+#names(cov)
 x11()
 chart.Correlation(cov[,c("ricoverati_con_sintomi","terapia_intensiva","isolamento_domiciliare",
-                          "nuovi_positivi","dimessi_guariti","deceduti","totale_casi",
-                            "tamponi","casi_testati")])
+                          "nuovi_positivi","dimessi_guariti","deceduti","perc_vax")])
+x11()
+chart.Correlation(cov[,c("ricoverati_con_sintomi","terapia_intensiva","isolamento_domiciliare",
+                         "nuovi_positivi","dimessi_guariti_dayly","deceduti_daily","perc_vax")])
+
+# Discuss together these results
+
+
+###############################################################################
+#                               FIT LM                                        #
+###############################################################################
 
 
 
-# # fit the complete linear model
-# lm1 <- lm(terapia_intensiva ~ ricoverati_con_sintomi+totale_ospedalizzati+isolamento_domiciliare+
-#          totale_positivi+nuovi_positivi+dimessi_guariti+deceduti+totale_casi+tamponi+casi_testati,
-#          data = cov)
-# lm1
-# 
-# #VIF
-# vif(lm1)
-# # we ran into perfect collinearity
-# 
-# x11()
-# avPlots(lm1,layout=c(2,5))
-# 
-# 
-# # we still have some correlation. We do a stepwise procedure to choose the model
-# 
-# lm2 <- lm(terapia_intensiva ~ totale_ospedalizzati+
-#             totale_positivi+nuovi_positivi+dimessi_guariti+totale_casi,
-#           data = cov)
-# summary(lm2)
-# 
-# 
-# 
+
+###############################################################################
+#                               FIT GLM                                       #
+###############################################################################
+
+
+
+
+
+###############################################################################
+#                               FIT GAM                                       #
+###############################################################################
+
 
 
 
