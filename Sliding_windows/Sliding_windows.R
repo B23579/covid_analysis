0270bd08db7813667ad86ee9e6a0b4f38265c230
WalterNadalin <64863148+WalterNadalin@users.noreply.github.com>
2022-02-05 21:58:57 +0100

    Linear analysis
    Fitted a linear model following the things done in the fourth laboratory

--- Sliding_windows/Sliding_windows.R
+++ Sliding_windows/Sliding_windows.R
@@ -75,7 +75,7 @@ cov <- merge(cov,color, by.x = "data", by.y = "data", all.x = TRUE)
 cov[is.na(cov)] <- "pre_decreto"
 cov$colore <- as.factor(cov$colore)
 
-# SHOW THE VARIABLES AND CHOOSE WHICH ONE TO CONSIDER #########################
+## SHOW THE VARIABLES AND CHOOSE WHICH ONE TO CONSIDER ########################
 
 # we can discard totale_positivi 
 cov <- dplyr::select(cov, -totale_ospedalizzati, -totale_positivi)
@@ -112,7 +112,7 @@ cov <- dplyr::select(cov, -deceduti_daily,-dimessi_guariti_dayly, -casi_testati_
 # sliding windows: this types of windows moves along the index
 # To obtain constant sliding windows we do the following
 
-# SLIDING WINDOWS: TOY EXAMPLE ################################################
+## TOY EXAMPLE #################################################################
 library(runner)
 
 # Example: sum of 4 elements considering the array [1, 2, 3, ..., 14, 15]
@@ -128,16 +128,7 @@ df <- data.frame(
   b = 3 * 1:15 + rnorm(15) # Second column 
 )
 
-runner(
-  x = df,
-  k = 5,
-  f = function(x) {
-    model <- lm(b ~ a, data = x)
-    coefficients(model)["a"]
-  }
-)
-
-## SLINDING WINDOWS: WINDOWS DEPENDING ON DATE ################################
+## SLINDING WINDOWS ############################################################
 # By default runner calculates on assumption that index increments by one, but 
 # sometimes data points in dataset are not equally spaced (missing weekends, 
 # holidays, other missings) and thus window size should vary to keep expected 
@@ -173,10 +164,10 @@ runner::runner(
 # SLIDING WINDOWS: ICU ########################################################
 
 # Let's add to our dataframe a feature that each day computes the means of the 
-# previous 7 days
+# previous 6 days
 sliding_ICU <- runner(
   cov$terapia_intensiva,
-  k = 7,
+  k = 6,
   f = mean
 )
 
@@ -184,10 +175,11 @@ cov$sliding_ICU <- sliding_ICU
 
 # Seems to work: let's plot it with respect to the date to see what I got
 ggplot(cov, aes(x = data, y = sliding_ICU, color = colore)) + 
-  scale_color_manual(values = c("orange", "green", "grey", "red")) +
+  scale_color_manual(values = c("orange", "light blue", "grey", "red")) +
   geom_path(aes(group = 1)) + 
   geom_point(size = 3, colour = "white") +
-  geom_point(size = 2.5) + theme_ipsum() # <- PLOT THIS
+  geom_point(size = 2.5) + theme_ipsum() # <- PLOT THIS 
+  # (Remark: now yellow is light blue because I can't see yellow)
 
 # Cool! There's seem to be a more smooth behavior with respect to before,
 # especially on January
@@ -209,16 +201,17 @@ cov <- dplyr::select(cov, -new_ICU_entries)
 # quite high
 cor(cov$terapia_intensiva, cov$sliding_ICU)
 
-# We can try to consider the 7 previous days (this implies that I have to
+# We can try to consider the 6 previous days (this implies that I have to
 # exclude the first point)
 shifted_sliding_ICU <- runner(
   cov$terapia_intensiva,
-  k = 7,
+  k = 6,
   lag = 1,
   f = mean
 )
 
 cov$shifted_sliding_ICU <- shifted_sliding_ICU
+cov <- dplyr::select(cov, -sliding_ICU)
 
 cov <- cov[-1, ] # Excluding the first point
 
@@ -226,3 +219,145 @@ cov <- cov[-1, ] # Excluding the first point
 # high correlation with 'terapia_intensiva' (lower than before though), but we 
 # now can use this variable for prediction
 cor(cov$terapia_intensiva, cov$shifted_sliding_ICU)
+
+## FITTING A LINEAR MODEL ######################################################
+chart.Correlation(dplyr::select(cov, -colore, -data,),  
+                  histogram = TRUE, method = "pearson")
+
+# Taking a look at the following correlation plot I think I'll consider only
+# the 4 variables in the chart to fit the linear model since it seems that they
+# are the only linearly correlated with 'terapia_intensiva' and I am already
+# considering 'terapia_intensiva_ieri' in some way in the 'sliding_ICU'
+chart.Correlation(dplyr::select(cov, -colore, -data, -perc_vax, 
+                                -dimessi_guariti, -terapia_intensiva_ieri,
+                                -deceduti, -isolamento_domiciliare),  
+                  histogram = TRUE, method = "pearson")
+
+# To warm up a bit and gain some confidence I start by considering a single
+# predictor: that is 'shifted_sliding_ICU'
+plot(cov$shifted_sliding_ICU, cov$terapia_intensiva, pch=19, xlab="Num", ylab="time")
+lm_ICU <- lm(terapia_intensiva ~ shifted_sliding_ICU, data = cov)
+par(mfrow = c(2, 2))
+plot(lm_ICU)
+
+# Not so good but whatever
+# Now I'll plot the fitted values
+par(mfrow = c(1, 1))
+with(cov, plot(shifted_sliding_ICU, terapia_intensiva, pch = 19))
+abline(coef(lm_ICU), col="red", lty="solid")
+
+text(70, 170, expression(terapia_intensiva == hat(beta)[0] + 
+                       hat(beta)[1] * shifted_sliding_ICU), col="red")
+
+points(cov$shifted_sliding_ICU, predict(lm_ICU), col = "red", 
+       pch = 19, cex = 0.8)
+
+segments(cov$shifted_sliding_ICU, cov$terapia_intensiva, 
+         cov$shifted_sliding_ICU, fitted(lm_ICU), lty="dashed")
+
+# Pretty nice plot, a final thing to notice
+summary(lm_ICU) # <- R^2 = 0.925
+
+# Indeed
+Tot_SS <- with(cov, sum((terapia_intensiva - mean(terapia_intensiva))^2)) 
+Res_SS <- with(cov, sum((predict(lm_ICU) - terapia_intensiva)^2))
+R_sq <- 1 - Res_SS/Tot_SS # <- 0.925
+
+# We can even visualize the results with respect to the date
+plot(cov$data, fitted(lm_ICU), type = "b", pch = 19, cex = 0.8, col = "red",
+     ylim = c(40, 220))
+points(cov$data, cov$terapia_intensiva,  type = "b", pch = 19, 
+       cex = 0.8, col = "black")
+segments(cov$data, cov$terapia_intensiva, 
+         cov$data, fitted(lm_ICU), lty = "dashed")
+
+# It seems to be shifted, this could be caused by the fact that I'm basing the
+# analysis on the measures of the previous days (?)
+
+### MULTIPLE LINEAR REGRESSION ################################################
+# We already visualized the correlation chart so now time to use the chosen
+# variable to get some goodies 
+lm_fantanstic_three <- lm(terapia_intensiva ~ nuovi_positivi +
+                          ricoverati_con_sintomi + shifted_sliding_ICU, 
+                          data = cov)
+par(mfrow = c(2,2))
+plot(lm_fantanstic_three)
+
+# Surely better than before, there are a couple of outliers though
+# Let's take a look at the R^2 for curiosity
+summary(lm_fantanstic_three) # R^2 --> 0.9657, yes: better
+
+# Obviously now we can't visualize a plot in 4 dimensions but we can still 
+# visualize it with respect to the date
+plot(cov$data, fitted(lm_fantanstic_three), type = "b", pch = 19, cex = 0.8,
+     col = "red", ylim = c(40, 220))
+points(cov$data, cov$terapia_intensiva,  type = "b", pch = 19, 
+       cex = 0.8, col = "black")
+segments(cov$data, cov$terapia_intensiva, 
+         cov$data, fitted(lm_fantanstic_three), lty = "dashed")
+
+# It better yes, but maybe it over fits the data a bit (?) 
+# In any case, I'm still using only a linear model so I can't expect much better
+# Just out of curiosity, let's also remove 'ricoverati_con_sintomì'
+lm_last <- lm(terapia_intensiva ~ nuovi_positivi + shifted_sliding_ICU, 
+              data = cov)
+plot(cov$data, fitted(lm_last), type = "b", pch = 19, cex = 0.8,
+     col = "red", ylim = c(40, 220))
+points(cov$data, cov$terapia_intensiva,  type = "b", pch = 19, 
+       cex = 0.8, col = "black")
+segments(cov$data, cov$terapia_intensiva, 
+         cov$data, fitted(lm_last), lty = "dashed")
+
+# And the R^2 is..
+summary(lm_last) # R^2 --> 0.9656, like the previous
+
+# Plotting the residuals against the common explanatory variables in the last 2
+# cases:  they're pretty much the same
+par(mfrow = c(2, 2))
+plot(cov$shifted_sliding_ICU, lm_last$residuals, ylab = 'last')
+abline(h = 0, lty = "dashed")
+plot(cov$shifted_sliding_ICU, lm_fantanstic_three$residuals, ylab = 'three')
+abline(h = 0, lty = "dashed")
+plot(cov$nuovi_positivi, lm_last$residuals, ylab = 'last')
+abline(h = 0, lty = "dashed")
+plot(cov$nuovi_positivi, lm_fantanstic_three$residuals, ylab = 'three')
+abline(h = 0, lty = "dashed")
+
+# Rad, this is even better than the one with 3 features, later I will compare
+# the models with better techniques
+# Really the last thing for this section: extending the simple linear regression 
+# model by modelling the explanatory variable dist as a polynomial of degree two
+# for 'nuovi_positivi', because looking at the correlation chart maybe there's
+# something (?)
+lm_polynomial <- lm(terapia_intensiva ~ nuovi_positivi + shifted_sliding_ICU + 
+                    I(nuovi_positivi^2), data = cov)
+
+par(mfrow = c(1, 1))
+lm_last <- lm(terapia_intensiva ~ nuovi_positivi + shifted_sliding_ICU, 
+              data = cov)
+plot(cov$data, fitted(lm_polynomial), type = "b", pch = 19, cex = 0.8,
+     col = "red", ylim = c(40, 220))
+points(cov$data, cov$terapia_intensiva,  type = "b", pch = 19, 
+       cex = 0.8, col = "black")
+segments(cov$data, cov$terapia_intensiva, 
+         cov$data, fitted(lm_polynomial), lty = "dashed")
+
+summary(lm_polynomial) # R^2 --> 0.9687
+# It is better, but is it correct?
+
+### AIC #######################################################################
+# AIC
+AIC <- rbind(extractAIC(lm_ICU)[2], extractAIC(lm_fantanstic_three)[2],
+             extractAIC(lm_last)[2], extractAIC(lm_polynomial)[2])
+
+AIC
+# The last model yields the lower AIC
+
+### RIDGE REGRESSION ##########################################################
+# Doing things randomly
+lm_ridge<- lm.ridge(terapia_intensiva ~ nuovi_positivi + shifted_sliding_ICU,
+                    lambda = 0, data = cov)
+
+# Remark: is a good idea to use different windows length for a different number
+# of people in ICU? 
+
